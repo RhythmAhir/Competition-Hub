c841e195d86f667bdee3bffde17eed702fc0487a
Rhythm Ahir <36238194+RhythmAhir@users.noreply.github.com>
2019-11-24 23:18:42 -0500

    Add files via upload

---
+++ Android Connection/includes/Constants.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+*
+* Contains important constants
+* 
+*/
+
+
+    define('DB_NAME', 'competitionhub_db');
+    define('DB_USER', 'root');
+    define('DB_PASSWORD', '');
+    define('DB_HOST', 'localhost');
